147a419b58bbfff4be1834ee2400ee5776dfc5fc
Poonyapat <42263205+Ponyapat@users.noreply.github.com>
2023-08-11 00:41:39 +0700

    Add files via upload

---
+++ MERN-Project/client/public/precache-manifest.fac5d290d56ecccc30b32630272d712f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce983c1297b47653e01590eb2c23556f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a59118fbc2968ea61357",
+    "url": "/static/css/main.b65f18d1.chunk.css"
+  },
+  {
+    "revision": "f470d17e4e81eec571df",
+    "url": "/static/js/2.0b3c03e2.chunk.js"
+  },
+  {
+    "revision": "c13bfd055a57809fb2d731409b713623",
+    "url": "/static/js/2.0b3c03e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59118fbc2968ea61357",
+    "url": "/static/js/main.dc314c03.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "c655e183fedfbdf06273bea023517bc1",
+    "url": "/static/media/logo2.c655e183.png"
+  },
+  {
+    "revision": "ec5468afce5102890179c2703965f682",
+    "url": "/static/media/picas.ec5468af.png"
+  }
+]);
